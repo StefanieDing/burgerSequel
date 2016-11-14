1fcacb834563868a628ca0ab6f0d39ff5a6acc1e
Stefanie Ding <StefanieDing@Stefanie-Dings-MacBook-Pro.local>
2016-11-13 19:42:13 -0500

    first deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var app = express();
-
+var connection = require('./config/connection.js');
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
 
