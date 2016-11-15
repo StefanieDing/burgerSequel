acddea7833cf5a6d3e147d18a1f29ae8bafd001a
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-11-14 23:38:34 -0500

    seeded menu data

--- seeders/20161113201858-burgers-seed.js
+++ seeders/20161113201858-burgers-seed.js
@@ -2,15 +2,15 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Burgers', [
-      { burger_name: 'Mushroom Swiss Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() },
-      { burger_name: 'Southwestern Veggie Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() },
-      { burger_name: 'Spinach Artichoke Turkey Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() },
-      { burger_name: 'Black & Bleu Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() }
-    ],  {});
+    // return queryInterface.bulkInsert('Burgers', [
+    //   { burger_name: 'Mushroom Swiss Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() },
+    //   { burger_name: 'Southwestern Veggie Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() },
+    //   { burger_name: 'Spinach Artichoke Turkey Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() },
+    //   { burger_name: 'Black & Bleu Burger', devoured: 0, createdAt: new Date(), updatedAt: new Date() }
+    // ],  {});
   },
 
   down: function (queryInterface, Sequelize) {
-     return queryInterface.bulkDelete('Burgers', null, {truncate : true});
+     // return queryInterface.bulkDelete('Burgers', null, {truncate : true});
   }
 };
